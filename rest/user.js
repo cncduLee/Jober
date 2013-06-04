834da5dc419716db188de2f766ce8cc4b5489e77
cncduLee <shouli1990@gmail.com>
2013-06-04 22:01:03 +0800

    restful interface (Gzip json)

--- rest/user.js
+++ rest/user.js
@@ -8,29 +8,44 @@ exports.restLogin = function(req, res,next){
 	
 	var result = {
 		type:"error",
-		content:'这是结果字符串！'
+		content:'这是结果字符串！',
+		id:"",
+		name:"",
+		pwd:"",
+		email:""
 	};
 
-	if (!config.rss) {
-	    res.statusCode = 404;
-	    result.content = 'Please set `rss` in config.js';
-	}
+	var email = req.query['email'];//trim(req.body['email']);
+	var password = req.query['password'];//trim(req.body['password']);
 
-	if(req.body['password'] ==='' || req.body['email'] === ''){
+	console.log(password + '=====' + email);
+
+	if(email==null || password==null || email =='' ||  password == ''){
 		result.content = '输入不能为空！';
+		return res.jsonp(result);
 	}
 
-	UserDao.getUserByEmail(req.body['email'],function(err,user){
+	UserDao.getUserByEmail(email,function(err,user){
 		
 		if(err){
-			result.content = 'result','数据库查询失败！';
+			result.content = '数据库维护中！';
+			return res.jsonp(result);
 		}
 
 		if(user){
-			if(user.password == req.body['password']){
+			if(user.password == password){
 				//登陆成功
 				result.type = 'success';
-				result.content = '登陆成功';		
+				result.content = '登陆成功';
+				
+				result.name = user.name;
+				result.pwd = user.password;
+				result.email = user.email;
+				result.id = user._id;
+
+				//注册到session
+				req.session.user = user;
+
 			}else{
 				//密码输入错误
 				result.content = '密码输入错误';
@@ -40,9 +55,12 @@ exports.restLogin = function(req, res,next){
 			result.content = '该用户不存在';
 		}
 
-		// var rest_content = data2xml('rss', result);
-	    // res.contentType('application/xml');
-	    res.send(result);
+		return res.jsonp(result);
 	});
 
 };
+
+/***删除左右两端的空格**/
+function trim(str){
+	return str.replace(/(^\s*)|(\s*$)/g, "");
+}
